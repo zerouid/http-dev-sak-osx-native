bb6ab0ee72ac1d62ab9ff88d3ac207ebb18b69f5
Ivan Minchev <ivan.minchev@blsdev0595lax-2.local>
2015-03-09 23:27:43 -0700

    Added a callback methods for reading process output and process termination

--- osx-native.js
+++ osx-native.js
@@ -1,38 +1,44 @@
 /*jslint node: true*/
 'use strict';
 
-var launch_priv = require('./build/Release/launch_priv');
+var launch_priv = require('./build/Release/launch_priv'),
+    debuglog = require('util').debuglog('osx-native');
 
 module.exports = {
-    launchPriviledged: function (cmd, args, success_callback, fail_callback) {
-        console.log('Current uid: ' + process.getuid());
+    launchPriviledged: function (cmd, args, progress, callback) {
+        debuglog('Current uid: ' + process.getuid());
         try {
             process.setuid(0);
-            console.log('New uid: ' + process.getuid());
+            debuglog('New uid: ' + process.getuid());
         } catch (err) {
-            console.log('Failed to set uid: ' + err);
+            debuglog('Failed to set uid: ' + err);
         }
         if (process.getuid() !== 0) {
             var cmd_line = cmd || process.execPath,
                 cmd_args = args || process.argv.slice(1).concat(process.execArgv),
-                result = launch_priv.launchPriv(cmd_line, cmd_args);
+                outputBuff = new Buffer(0),
+                result = launch_priv.launchPriv(cmd_line, cmd_args, function (buff) {
+                    debuglog('received: ' + buff);
+                    if (typeof progress === 'function') {
+                        progress(buff);
+                    }
+                }, function () {
+                    debuglog('Done.');
+                    if (typeof callback === 'function') {
+                        callback(null);
+                    }
+                });
 
-            console.log('cmd_line: ' + cmd_line + ' ' + cmd_args.join());
-            console.log('result:' + result);
-            if (result === 0) {
-                if (typeof success_callback === 'function') {
-                    success_callback();
-                } else {
-                    process.exit(0);
-                }
-            } else {
-                console.log('ERROR!');
-                if (typeof fail_callback === 'function') {
-                    fail_callback(result);
+            debuglog('cmd_line: ' + cmd_line + ' ' + cmd_args.join());
+            debuglog('result:' + result);
+            if (result !== 0) {
+                debuglog('ERROR: ' + result);
+                if (typeof callback === 'function') {
+                    callback(result);
                 }
             }
         } else {
-            console.log('ALLREADY ROOT!');
+            debuglog('ALLREADY ROOT!');
         }
     }
 };
--- src/osx_native.cc
+++ src/osx_native.cc
@@ -2,77 +2,137 @@
 #include <Security/Security.h>
 #include <SystemConfiguration/SystemConfiguration.h>
 
+class PipeWorker : public NanAsyncProgressWorker {
+    public:
+        PipeWorker(
+            NanCallback *callback,
+            NanCallback *progress,
+            FILE *pipe) : NanAsyncProgressWorker(callback), progress(progress), pipe(pipe) {}
+        ~PipeWorker() {
+            fclose(pipe);
+        }
+
+    void Execute (const NanAsyncProgressWorker::ExecutionProgress& progress) {
+        int bufferSize = 256;
+        char buffer[bufferSize];
+
+        if(pipe) {
+            for(;;) {
+                int bytesRead = read (fileno (pipe), buffer, bufferSize);
+                if (bytesRead < 1)
+                    break;
+                else
+                    progress.Send(buffer, bytesRead);
+            }
+        }
+    }
+
+    void HandleProgressCallback(const char *data, size_t size) {
+        NanScope();
+
+        v8::Local<v8::Value> argv[] = {
+            NanNewBufferHandle(data, size)
+        };
+
+        progress->Call(1, argv);
+    }
+
+    private:
+        NanCallback *progress;
+        FILE *pipe;
+};
+
 
 NAN_METHOD(LaunchPrivilegedProcess) {
-  NanScope();
-  if(args.Length() < 1 && args.Length() > 2) {
-    NanThrowTypeError("Wrong number of arguments");
-    NanReturnUndefined();
-  }
-  if(!args[0]->IsString()) {
-    NanThrowTypeError("Argument must be a String");
-    NanReturnUndefined();
-  }
-  if(args.Length() == 2 && !args[1]->IsArray()) {
-    NanThrowTypeError("Argument must be a Array");
-    NanReturnUndefined();
-  }
-
-  NanUtf8String path(args[0]->ToString());
-  char **myArguments = NULL;
-  FILE *myCommunicationsPipe = NULL;
-  if(args.Length() == 2) {
-    v8::Local<v8::Array> arr = v8::Local<v8::Array>::Cast(args[1]);
-    int length = arr->Length();
-    myArguments = (char**)malloc((length + 1) * sizeof(char*));
-    for(int i = 0; i < length; i++)
-    {
-      NanUtf8String strval(arr->Get(i)->ToString());
-      myArguments[i] = (char *) malloc(strval.length() + 1);
-      strcpy(myArguments[i], *strval);
+    NanScope();
+    if(args.Length() < 1 && args.Length() > 2) {
+        NanThrowTypeError("Wrong number of arguments");
+        NanReturnUndefined();
     }
-    myArguments[length] = NULL;
-  }
 
-  AuthorizationRef authRef;
-  char myReadBuffer[128];
+    if(!args[0]->IsString()) {
+        NanThrowTypeError("Argument must be a String");
+        NanReturnUndefined();
+    }
 
-  OSStatus status;
-  status = AuthorizationCreate(NULL, kAuthorizationEmptyEnvironment, kAuthorizationFlagDefaults, &authRef);
-  if (status == errAuthorizationSuccess) {
-    AuthorizationItem items = {kAuthorizationRightExecute, 0, NULL, 0};
-    AuthorizationRights rights = {1, &items};
+    if(args.Length() > 1 && !args[1]->IsArray()) {
+        NanThrowTypeError("Argument must be a Array");
+        NanReturnUndefined();
+    }
 
-    status = AuthorizationCopyRights (authRef, &rights, NULL, kAuthorizationFlagDefaults |
+    NanCallback *progress = NULL;
+    if(args.Length() > 2 && args[2]->IsFunction()) {
+        progress = new NanCallback(args[2].As<v8::Function>());
+    }
+
+    NanCallback *callback = NULL;
+    if(args.Length() > 3 && args[3]->IsFunction()) {
+        callback = new NanCallback(args[3].As<v8::Function>());
+    }
+
+    char **myArguments = NULL;
+    FILE *myCommunicationsPipe = NULL;
+
+
+    NanUtf8String path(args[0]->ToString());
+    if(args.Length() >= 2) {
+        v8::Local<v8::Array> arr = v8::Local<v8::Array>::Cast(args[1]);
+        int length = arr->Length();
+
+        myArguments = (char**)malloc((length + 1) * sizeof(char*));
+        for(int i = 0; i < length; i++) {
+            NanAsciiString strval(arr->Get(i)->ToString());
+            myArguments[i] = (char *) malloc(strval.length() + 1);
+            strcpy(myArguments[i], *strval);
+        }
+        myArguments[length] = NULL;
+    }
+
+    AuthorizationRef authRef;
+
+    OSStatus status = AuthorizationCreate(NULL,
+                                          kAuthorizationEmptyEnvironment,
+                                          kAuthorizationFlagDefaults,
+                                          &authRef);
+
+    if (status == errAuthorizationSuccess) {
+        AuthorizationItem items = {kAuthorizationRightExecute, 0, NULL, 0};
+        AuthorizationRights rights = {1, &items};
+
+        status = AuthorizationCopyRights(authRef, &rights, NULL,
+                                      kAuthorizationFlagDefaults |
                                       kAuthorizationFlagInteractionAllowed |
                                       kAuthorizationFlagPreAuthorize |
                                       kAuthorizationFlagExtendRights, NULL );
-    if (status == errAuthorizationSuccess) {
-      status = AuthorizationExecuteWithPrivileges(authRef, (char *)*path, kAuthorizationFlagDefaults, myArguments, &myCommunicationsPipe);
-      if (status == errAuthorizationSuccess)
-        for(;;)
-      {
-        int bytesRead = read (fileno (myCommunicationsPipe),myReadBuffer, sizeof (myReadBuffer));
-        if (bytesRead < 1) break;
-        write (fileno (stdout), myReadBuffer, bytesRead);
-      }
-    }
-  }
 
-  if(myArguments) {
-    for(int i=0; myArguments[i] != NULL; i++) {
-      free(myArguments[i]);
+        if (status == errAuthorizationSuccess) {
+            status = AuthorizationExecuteWithPrivileges(authRef,
+                                                        (char *)*path,
+                                                        kAuthorizationFlagDefaults,
+                                                        myArguments,
+                                                        &myCommunicationsPipe);
+
+            if (status == errAuthorizationSuccess && callback != NULL && progress != NULL) {
+                NanAsyncQueueWorker(new PipeWorker(callback, progress, myCommunicationsPipe));
+            }
+        }
     }
 
-    free(myArguments);
-  }
-  AuthorizationFree(authRef, kAuthorizationFlagDefaults);
-  NanReturnValue(NanNew(status));
+    if(myArguments) {
+        for(int i=0; myArguments[i] != NULL; i++) {
+            free(myArguments[i]);
+        }
+        free(myArguments);
+    }
+
+    AuthorizationFree(authRef, kAuthorizationFlagDefaults);
+
+    NanReturnValue(NanNew(status));
 }
 
 void Init(v8::Handle<v8::Object> exports) {
-  exports->Set(NanNew("launchPriv"),
-               NanNew<v8::FunctionTemplate>(LaunchPrivilegedProcess)->GetFunction());
+    exports->Set(NanNew("launchPriv"),
+                 NanNew<v8::FunctionTemplate>(LaunchPrivilegedProcess)->GetFunction());
 }
 
 NODE_MODULE(launch_priv, Init)
